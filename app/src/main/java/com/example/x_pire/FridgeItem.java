b1fc62b83c4edeb7200f2f08cb34fcb9aa6fae7d
Brian Tran <btran0014@gmail.com>
2024-03-02 04:13:37 -0500

    item listing is complete(?)

--- app/src/main/java/com/example/x_pire/FridgeItem.java
+++ app/src/main/java/com/example/x_pire/FridgeItem.java
@@ -1,4 +1,36 @@
 package com.example.x_pire;
 
 public class FridgeItem {
+    private String itemName, itemLogDate, itemExpiryDate, itemID;
+
+    public FridgeItem(String itemID, String itemName, String itemLogDate, String itemExpiryDate){
+        this.itemID = itemID;
+        this.itemName = itemName;
+        this.itemLogDate = itemLogDate;
+        this.itemExpiryDate = itemExpiryDate;
+    }
+    public void setItemName(String input){
+        this.itemName = input;
+    }
+
+    public void setItemLogDate(String input){
+        this.itemLogDate = input;
+    }
+
+    public void setItemExpiryDate(String input){
+        this.itemExpiryDate = input;
+    }
+
+    public String getItemName(){
+        return itemName;
+    }
+    public String getItemLogDate(){
+        return itemLogDate;
+    }
+    public String getItemExpiryDate(){
+        return itemExpiryDate;
+    }
+    public String getItemID(){
+        return itemID;
+    }
 }
